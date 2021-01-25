ca9a34caa9ca5f0ea9d909bfddb7d3b4f6581bd8
Maksim Solopov <mxsolopov@yandex.ru>
2021-01-25 15:59:52 +0300

    Added gift viewing

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -3,26 +3,40 @@ import calcScroll from './calcScroll';
 
 const modals = () => {
 
+    // Индикатор кликов на кнопки
+    let btnPressed = false;
+
     // Функция показа модальных окон
-    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = 'true') {
+    function bindModal(triggerSelector, modalSelector, closeSelector, destroy = 'false') {
 
         const trigger = document.querySelectorAll(triggerSelector),
               modal = document.querySelector(modalSelector),
               close = document.querySelector(closeSelector),
-              popup = document.querySelectorAll('[data-modal]'),
+              popupSet = document.querySelectorAll('[data-modal]'),
               scroll = calcScroll();
 
         // Показать окно при клике на триггер
         for (let key of trigger) {
             key.addEventListener('click', (e) => {
 
-                closeModals(popup);
+                // Пользователь кликнул на кнопку
+                btnPressed = true;
+
+                closeModals(popupSet);
 
                 if (e.target) {
                     e.preventDefault();
                 }
-    
+
+                // Убирать элемент, по которому делается клик
+                if (destroy) {
+                    key.remove();
+                }
+                
+
                 modal.style.display = 'block';
+                // Плавное появление
+                modal.classList.add('animated', 'fadeIn');
                 document.body.style.overflow = 'hidden';
                 document.body.style.marginRight = `${scroll}px`;
             });
@@ -31,7 +45,7 @@ const modals = () => {
         // Скрыть окно при клике на крестик
         close.addEventListener('click', () => {
 
-            closeModals(popup);
+            closeModals(popupSet);
 
             modal.style.display = 'none';
             document.body.style.overflow = '';
@@ -40,9 +54,9 @@ const modals = () => {
 
         // Скрыть окно при клике на область вне формы
         modal.addEventListener('click', (e) => {
-            if (e.target === modal && closeClickOverlay) {
+            if (e.target === modal) {
 
-                closeModals(popup);
+                closeModals(popupSet);
 
                 modal.style.display = 'none';
                 document.body.style.overflow = '';
@@ -68,15 +82,32 @@ const modals = () => {
             if (!display) {
                 document.querySelector(selector).style.display = 'block';
                 document.body.style.overflow = "hidden";
+                const scroll = calcScroll();
                 document.body.style.marginRight = `${scroll}px`;
             }
         }, time);
     }
 
+    // Показ модальной формы при скролле до конца страницы
+    function openByScroll(selector) {
+        window.addEventListener('scroll', () => {
+
+            let scrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
+
+            // Имитация клика
+            if (!btnPressed && (window.pageYOffset + document.documentElement.clientHeight >= scrollHeight)) {
+                document.querySelector(selector).click();
+            }
+        });
+    }
+
     // Открытие модальных форм проекта
     bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
     bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
+    bindModal('.fixed-gift', '.popup-gift', '.popup-gift .popup-close', 'true');
     // showModalByTime('.popup-consultation', 5000);
+    // Показать подарок при долистывании до конца страницы
+    openByScroll('.fixed-gift');
 };
 
 export default modals;
